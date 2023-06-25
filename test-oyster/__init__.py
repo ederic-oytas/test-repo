895543128c576e86f93c75b3262ba5c58141cce4
Ederic <edericoytas@gmail.com>
2023-06-24 18:42:19 -0700

    Create __init__.py and create `add` function

---
+++ test-oyster/__init__.py
@@ -0,0 +1,13 @@
+
+
+def add(a: int, b: int) -> int:
+    """Adds two integers together.
+    
+    Args:
+        a: First argument.
+        b: Second argument.
+    
+    Returns:
+        The sum of the two integers.
+    """
+    return a + b
