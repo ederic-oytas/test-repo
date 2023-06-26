0b2f760ad5d308bf1aaf5c5c6089f8af3179acf8
Ederic <edericoytas@gmail.com>
2023-06-26 15:42:52 -0700

    Add `sum_1d_array` function

--- test_oyster/adding.py
+++ test_oyster/adding.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def add(a: int, b: int) -> int:
     """Adds two integers together.
 
@@ -9,3 +12,18 @@ def add(a: int, b: int) -> int:
         The sum of the two integers.
     """
     return a + b
+
+
+def sum_1d_array(a: np.ndarray) -> float:
+    """Returns the sum of the given 1-dimensional array.
+
+    Args:
+        a: 1-dimensional array of real numbers.
+
+    Returns:
+        The sum of the 1D array.
+    """
+    total = 0.0
+    for i in range(a.shape[0]):
+        total += a[i]
+    return total
