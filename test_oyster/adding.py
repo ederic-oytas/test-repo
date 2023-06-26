a85a34b38bd228664ce744f0b97980682b227d52
Ederic <edericoytas@gmail.com>
2023-06-26 12:24:17 -0700

    Reformat with black

--- test_oyster/adding.py
+++ test_oyster/adding.py
@@ -1,11 +1,10 @@
-
 def add(a: int, b: int) -> int:
     """Adds two integers together.
-    
+
     Args:
         a: First argument.
         b: Second argument.
-    
+
     Returns:
         The sum of the two integers.
     """
